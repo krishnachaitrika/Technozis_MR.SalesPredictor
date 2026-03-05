7b1b08543c2fb10744102f26066e9ef51d4162f1
krishna chaitrika <kollakrishnachaitrika6679@gmail.com>
2026-03-05 20:05:56 +0530

    Add files via upload

--- models/model.py
+++ models/model.py
@@ -7,25 +7,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import joblib
 from src.preprocess import preprocess_data
-
-# load and preprocess
 df = preprocess_data("../data/Walmart.csv")
 
-# features and target
 X = df.drop(columns=['Weekly_Sales'])
 y = df['Weekly_Sales']
 
-# split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# train model
-model = RandomForestRegressor(n_estimators=100)
+model = RandomForestRegressor(
+    n_estimators=50,
+    max_depth=10,
+    random_state=42
+)
 
 model.fit(X_train, y_train)
 
-# save model
 joblib.dump(model, "model.pkl")
 
 print("Model trained successfully")
